c4a282e1992a2143d4c738f8e559e0174c94ce09
YXCZ <minjing@mc2050.com>
2017-01-11 14:49:22 +0800

    Initial Commit

---
+++ MJEmptyView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJEmptyView
+//
+//  Created by YXCZ on 17/1/11.
+//  Copyright © 2017年 林民敬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MJEmptyView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MJEmptyView
+//
+//  Created by YXCZ on 17/1/11.
+//  Copyright © 2017年 林民敬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
